93173387f2eb321fc29c8a0f04e8bf5d992124e3
SnehaSandipBobade <snhabobade@gmail.com>
2025-05-08 20:17:35 +0530

    Commit trial

---
+++ src/ex_01_Java_Basics/Lab004.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        int num=5,i=1,res=0;
+        System.out.println("Table of 5 :");
+        for(i=1;i<=10;i++){
+            res=num*i;
+            System.out.println(num+"*"+i+"="+res);
+        }
+
+    }
+}
