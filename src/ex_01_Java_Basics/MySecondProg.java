787ce6a2b7651b51b0dd92ce5b779770253e9d46
SnehaSandipBobade <snhabobade@gmail.com>
2025-05-08 20:17:35 +0530

    Commit trial

---
+++ src/ex_01_Java_Basics/MySecondProg.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class MySecondProg {
+
+    public static void main(String[] args){
+
+       System.out.println("Hi, This is test prog to test commit");
+    }
+}
