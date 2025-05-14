b63a2ff5cda2e2a5019a2c6510ff6622049c7b24
SnehaSandipBobade <snhabobade@gmail.com>
2025-05-14 19:22:52 +0530

    Commit on 14th may

---
+++ src/ex_01_Java_Basics/Lab002.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Can we run a prog without main method in java");
+        System.out.println("If JDK version below 7 we are using it is possible  ");
+        System.out.println("If JDK version 8 to 21 we have we can not run without main ");
+        System.out.println("Above 21 is there is way to run using static");
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab003.java
@@ -0,0 +1,22 @@
+package ex_01_Java_Basics;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+        int a=2,b=3,c=1,d,e,f,g,h;
+        d=a+b;
+        e=b-a;
+        f=a*b;
+        g=a/b;
+        h=b/a;
+        System.out.println("Basic math with result");
+        System.out.println("a="+a+"\t"+"b="+b);
+        System.out.println("a+b="+d);
+        System.out.println("b-a="+e);
+        System.out.println("a*b="+f);
+        System.out.println("a/b="+g);
+        System.out.println("b/a="+h);
+        System.out.println("Done");
+
+    }
+}
