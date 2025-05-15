a99ea6dff3b1f90c8d501d4ef915884b777e0b06
SnehaSandipBobade <snhabobade@gmail.com>
2025-05-15 18:53:37 +0530

    table of 5 using printf 14-05-2025

---
+++ src/ex_03_JavaTasks/Lab022_task1.java
@@ -0,0 +1,63 @@
+package ex_03_JavaTasks;
+
+import java.util.Arrays;
+
+public class Lab022_task1 {
+    public static void main(String[] args) {
+        int num = 5;
+        int i=1;
+        int res=0;
+        System.out.println("Table of 5 using printf: ");
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+        i++;
+        res=num*i;
+        System.out.printf("|%d * %d| = ",num,i);
+        System.out.printf("%d",res);
+        System.out.print("\n");
+//        i++;
+//        res=num*i;
+    }
+}
