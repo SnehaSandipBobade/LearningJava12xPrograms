42779e12ee0d579748c3e3f62fd61f7734b0cfd2
SnehaSandipBobade <snhabobade@gmail.com>
2025-05-20 14:42:15 +0530

    Assignment Operator commit

---
+++ src/ex_04_Java_Operator/Lab027_AssignmentOper.java
@@ -0,0 +1,18 @@
+package ex_04_Java_Operator;
+
+public class Lab027_AssignmentOper {
+
+    public static void main(String[] args) {
+
+        int age = 90;
+        // Assignment operator
+        // int -> data type
+        // age -> identifier
+        // = -> Assignment operator
+        // 90 -> Literal
+        System.out.println("age is int and value assign using operator '=' is "+age);
+        // RHV to the LH
+
+
+    }
+}
