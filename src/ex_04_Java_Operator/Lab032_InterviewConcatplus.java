8af856d0ad72280b423ecfc46585d018e67a7527
SnehaSandipBobade <snhabobade@gmail.com>
2025-05-20 15:22:54 +0530

    Concat Operator

---
+++ src/ex_04_Java_Operator/Lab032_InterviewConcatplus.java
@@ -0,0 +1,21 @@
+package ex_04_Java_Operator;
+
+public class Lab032_InterviewConcatplus {
+    public static void main(String[] args) {
+        // concatenation
+        System.out.println("give th example of operator overloading ");
+        System.out.println("how + operator work with string and ");
+        String s1 = "Sneha";
+        String s2 = "Sandip";
+        System.out.println(s1 + s2); // Joins
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b); // Math
+
+        // + -> behave differently with the data type.
+        // + -> operator overloading
+
+
+    }
+}
