0210a3fb11140720e6b3e909570d63cc07da9d78
SnehaSandipBobade <snhabobade@gmail.com>
2025-06-12 21:04:44 +0530

    Fun commit

---
+++ src/ex_13_Functions/Lab120_Functions1.java
@@ -0,0 +1,9 @@
+package ex_13_Functions;
+
+public class Lab120_Functions1 {
+    public static void main(String[] args) {
+        int result = Math.min(3,4);
+        System.out.println(result);
+    }
+
+}
