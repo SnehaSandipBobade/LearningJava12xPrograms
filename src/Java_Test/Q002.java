084957626fabdf0ca69c3ea1fa9c2dc2ef2cf307
SnehaSandipBobade <snhabobade@gmail.com>
2025-06-21 11:44:51 +0530

    Q002 commit

---
+++ src/Java_Test/Q002.java
@@ -0,0 +1,26 @@
+package Java_Test;
+
+public class Q002 {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+        //        + (Addition)
+        //       - (Subtraction)
+        //       * (Multiplication)
+        //       / (Division)
+        //       % (Modulus) | Modulus - Remainder
+        int a = 20;
+        int b = 3;
+        float c = 3.0f;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        System.out.println(a%c);
+
+
+
+    }
+}
