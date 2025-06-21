ac8c19b5bf17d335abb1b2bcf93ed1d533f1ba89
SnehaSandipBobade <snhabobade@gmail.com>
2025-06-21 11:41:44 +0530

    Q001 commit

---
+++ src/Java_Test/Q001.java
@@ -0,0 +1,21 @@
+package Java_Test;
+
+public class Q001 {
+    public static void main(String[] args) {
+        int productQuantity = 100;
+        float pi= 3.14F;
+        double itemPrice = 25.99;
+        boolean isActive = true;
+        char initialLetter = 'J';
+        long longvalue= 12345;
+
+        System.out.println("Variable Declaration and Initialization");
+        System.out.println("Integer Variable (productQuantity): " + productQuantity);
+        System.out.println("Float Variable (pi): " + pi);
+        System.out.println("Double Variable (itemPrice): " + itemPrice);
+        System.out.println("Boolean Variable (isActive): " + isActive);
+        System.out.println("Character Variable (initialLetter): " + initialLetter);
+        System.out.println("long Variable (longvalue): " + longvalue);
+
+    }
+}
