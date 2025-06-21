a8f37d1621e470280143ff44f22a55b0c9266be1
SnehaSandipBobade <snhabobade@gmail.com>
2025-06-21 12:48:01 +0530

    Q10

---
+++ src/Java_Test/Q010.java
@@ -0,0 +1,18 @@
+package Java_Test;
+
+import java.util.Scanner;
+
+public class Q010 {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter no till u want print");
+            int n = sc.nextInt();
+            System.out.println("Even numbers between 1-100");
+            for (int i =1;i<=n;i++)
+            {
+                    System.out.println(i);
+            }
+        }
+
+}
