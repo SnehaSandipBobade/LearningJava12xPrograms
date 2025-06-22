4766124fbf8a595a6fd36b2d19aabca9d4818049
SnehaSandipBobade <snhabobade@gmail.com>
2025-06-22 11:25:44 +0530

    Q12

---
+++ src/Java_Test/Person.java
@@ -0,0 +1,12 @@
+package Java_Test;
+
+public class Person {
+    public static void main(String[] args) {
+        String name="sneha";
+        int age = 100;
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+    }
+
+
+}
