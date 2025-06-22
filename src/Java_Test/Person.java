b097a3acc88d3057368d62a62a634ac67959d417
SnehaSandipBobade <snhabobade@gmail.com>
2025-06-22 11:30:49 +0530

    Persn

--- src/Java_Test/Person.java
+++ src/Java_Test/Person.java
@@ -1,11 +1,26 @@
 package Java_Test;
 
 public class Person {
+    String name="sneha";
+    int age = 100;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("A new Person object created: " + name);
+    }
+    public void printDetails() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
     public static void main(String[] args) {
-        String name="sneha";
-        int age = 100;
-        System.out.println("Name : "+name);
-        System.out.println("Age : "+age);
+        Person person1 = new Person("Sneha", 30);
+        Person person2 = new Person("Sandip", 35);
+        System.out.print("Details for Person 1: ");
+        person1.printDetails();
+
+
+        System.out.print("Details for Person 2: ");
+        person2.printDetails();
+
     }
 
 
